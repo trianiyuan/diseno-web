55e0ccec921ef392e65ef6ec8318b006772f4339
trianiyuan <triani001@gmail.com>
2024-03-21 13:01:41 -0600

    avance tarea 3

---
+++ tarea3/js/scrollup.js
@@ -0,0 +1,27 @@
+//desaparece el boton ScrollTop al cargar la pagina web
+document.getElementById("flechabtn").style.display = "none";
+
+//esconder botón ScrollTop (parte superior)
+window.onscroll = function() {
+    scrollFunction()
+};
+
+//funcion scroll
+function scrollFunction() {
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+        document.getElementById("flechabtn").style.display = "block";
+    } else {
+        document.getElementById("flechabtn").style.display = "none";
+    }
+}
+
+
+
+
+//Funcián Scrolltop (Jquery)
+function topFunction() {
+    $('a.scroll-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 });
+    });
+}
